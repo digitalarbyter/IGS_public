2183db33ff4b1b785180f25dfab3c028234a3433
Digitalarbyter <okluth@gmail.com>
2018-06-03 16:03:21 +0200

    Read Instagram account info
    added biography and number of uploads

--- grab_ig_data2.php
+++ grab_ig_data2.php
@@ -6,5 +6,7 @@ $data_json = explode(';</script>', $data[1]);
 $data_array = json_decode($data_json[0], TRUE);
 $followers=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_followed_by']['count'];
 $followed_by=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_follow']['count'];
-echo $followers." / ".$followed_by;
+$biography=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['biography'];
+$uploads=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['count'];
+echo $followers." / ".$followed_by." / ".$uploads." / ".$biography."<br /><br />";
 ?>
