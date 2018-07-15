63d2394b7b9a52b718a1059bcada14dc5f0064b7
Digitalarbyter <okluth@gmail.com>
2018-07-15 15:26:08 +0200

    Added error-handling if profile doesn't exist
    Checking if the http-status of a profile-url returns any errors and only goes on if there aren't any problems with the url.

--- grab_ig_data2.php
+++ grab_ig_data2.php
@@ -1,15 +1,22 @@
 <?php
 $username = 'digitalarbyter';
-$ig_data = file_get_contents('http://instagram.com/'.$username);
-$data = explode('window._sharedData = ', $ig_data);
-$data_json = explode(';</script>', $data[1]);
-$data_array = json_decode($data_json[0], TRUE);
-$followers=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_followed_by']['count'];
-$followed_by=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_follow']['count'];
-$biography=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['biography'];
-$uploads=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['count'];
-$profile_pic=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['profile_pic_url'];
-$profile_pic_hd=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['profile_pic_url_hd'];
-$username=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['username'];
-echo $username." / ".$followers." / ".$followed_by." / ".$uploads." / ".$biography;
+$url="http://instagram.com/".$username;
+$ig_data = @file_get_contents($url);
+
+if($ig_data === false)
+{
+  echo "Error with Instagram profile!";
+} else {
+  $data = explode('window._sharedData = ', $ig_data);
+  $data_json = explode(';</script>', $data[1]);
+  $data_array = json_decode($data_json[0], TRUE);
+  $followers=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_followed_by']['count'];
+  $followed_by=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_follow']['count'];
+  $biography=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['biography'];
+  $uploads=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['count'];
+  $profile_pic=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['profile_pic_url'];
+  $profile_pic_hd=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['profile_pic_url_hd'];
+  $username=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['username'];
+  echo $username." / ".$followers." / ".$followed_by." / ".$uploads." / ".$biography;
+}
 ?>
