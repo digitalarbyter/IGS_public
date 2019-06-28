ba0228a40afc937b5ec496eaa729926ab31b438d
Digitalarbyter <okluth@gmail.com>
2019-06-28 21:56:03 +0200

    update

--- grab_ig_data2.php
+++ grab_ig_data2.php
@@ -22,9 +22,26 @@ if($ig_data === false)
   $user_is_private=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['is_private'];
   $user_is_verified=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['is_verified'];
   $user_id=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['id'];
-  $post_comments=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['edge_media_to_comment']['count'];
-  $post_likes=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['edge_liked_by']['count'];
+  $ig_user_biography=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['biography'];
+  $post_likes=array();
+  $post_comments=array();
+  $post_shortcode=array();
+  for($i=0;$i<=20;$i++)
+  {
+  $post_comments[$i]=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][$i]['node']['edge_media_to_comment']['count'];
+  $post_likes[$i]=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][$i]['node']['edge_liked_by']['count'];
+  $post_shortcode[$i]=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][$i]['node']['shortcode'];
+  }
 
-  echo $username." / ".$followers." / ".$followed_by." / ".$uploads." / ".$biography." / ".$user_id." / ".$user_external_url." / ".$user_full_name." / ".$user_is_private." / ".$user_is_verified." / ".$post_comments." / ".$post_likes;
+  for($j=0;$j<=$i;$j++)
+  {
+    echo $j." ".$post_shortcode[$j]." ".$post_likes[$j]." ".$post_comments[$j]."<br />";
+  }
+  $bulk= $data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['id'];
+  $bulk2=$data_array['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'];
+  echo $username." / ".$followers." / ".$followed_by." / ".$uploads." / ".$biography." / ".$user_id." / ".$user_external_url." / ".$user_full_name." / ".$user_is_private." / ".$user_is_verified." / ".$post_comments." / ".$post_likes." / ".$post_shortcode;
+  echo "<br /><br />";
+  //print_r($bulk2);
 }
+echo nl2br(utf8_decode($ig_user_biography));
 ?>
